c15957d0df5ff7f2520210f936be35eea68f78d9
Sumit-Kanpartiwar <98641781+Sumit-Kanpartiwar@users.noreply.github.com>
2022-03-13 18:33:16 +0530

    Update insert_data_in_mongo.py

--- insert_data_in_mongo.py
+++ insert_data_in_mongo.py
@@ -1,8 +1,26 @@
+import pymongo
 import csv
 import logging
 logging.basicConfig(filename='mongo.log',level=logging.DEBUG,format='%(asctime)s')
 
-with open('carbon_nanotubes.csv','r') as file:
+try: 
+    logging.info("trying to initiate connection with mongo db server")
+    client = pymongo.MongoClient("mongodb+srv://mongodb:mongodb@cluster0.nlmjr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    db = client.test
+    db=client['dataBase']
+    coll=db['Collection']
+except Exception as e:
+    logging.error(f"an error occured while connecting with mongo server:{e}")
+
+def insert_into_mongo(dict_a):
+    """A function that insert input argument dictionary as document in collection"""
+    try: 
+        coll.insert_one(dict_a)
+    except Exception as e:
+        logging.error(f"an error occured:{e}")
+
+filename='carbon_nanotubes.csv' #csv file from which the data is inserted to mongodb server
+with open(filename,'r') as file:
     csvfile=csv.reader(file, delimiter=";")
     dict_u={}
     logging.info("trying to read the csv file and intiating dictioary to insert into the mongodb Database")
@@ -19,6 +37,7 @@ with open('carbon_nanotubes.csv','r') as file:
             dict_u['_id']=x
             for j,key in zip(i,dict_u.keys()):
                 dict_u[key]=j.split(',')
-            coll.insert_one(dict_u)
+            insert_into_mongo(dict_u)
     except Exception as e:
-        print(f"an error occured {e}")
+        logging.error(f"an error occured:{e}")
+            
